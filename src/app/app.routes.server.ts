d32b701116c7b481fe2553ed3f4eaaefa54a0549
NDP <ndp@email.com>
2026-04-28 17:00:56 +0700

    update

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,62 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
+    path: '',
     renderMode: RenderMode.Prerender
+  },
+
+  {
+    path: 'tiktok-downloader',
+    renderMode: RenderMode.Prerender
+  },
+
+  {
+    path: 'facebook-video-download',
+    renderMode: RenderMode.Prerender
+  },
+
+  {
+    path: 'instagram-downloader',
+    renderMode: RenderMode.Prerender
+  },
+
+  {
+    path: 'xiaohongshu-downloader',
+    renderMode: RenderMode.Prerender
+  },
+
+  {
+    path: 'about-us',
+    renderMode: RenderMode.Prerender
+  },
+
+  {
+    path: 'privacy-policy',
+    renderMode: RenderMode.Prerender
+  },
+
+  {
+    path: 'terms-of-service',
+    renderMode: RenderMode.Prerender
+  },
+
+  {
+    path: 'contact',
+    renderMode: RenderMode.Prerender
+  },
+
+  {
+    path: 'blogs',
+    renderMode: RenderMode.Prerender
+  },
+
+  {
+    path: 'blogs/:id',
+    renderMode: RenderMode.Server
+  },
+
+  {
+    path: '**',
+    renderMode: RenderMode.Server
   }
 ];
